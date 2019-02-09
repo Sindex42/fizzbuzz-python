cfad193d07d865cae2f2fa3040c73802ce7469fa
Connor Kam-Cheong <connorkc42@gmail.com>
2019-02-09 21:36:34 +0000

    Fizzbuzz returns fizz or buzz when multiples of 3 or 5 respectively

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,7 @@
 def fizzbuzz(x):
-    if x == 3:
+    if x % 3 == 0:
         return 'fizz'
-    elif x == 5:
+    elif x % 5 == 0:
         return 'buzz'
     else:
         return x
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -9,3 +9,9 @@ def test_fizzbuzz_returns_buzz():
 
 def test_fizzbuzz_returns_x():
     assert fizzbuzz(1) == 1
+
+def test_fizzbuzz_returns_fizz_when_multiple_of_3():
+    assert fizzbuzz(6) == 'fizz'
+
+def test_fizzbuzz_returns_buzz_when_multiple_of_5():
+    assert fizzbuzz(10) == 'buzz'
