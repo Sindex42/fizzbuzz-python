7abc6f166dd82707fe7cffc12443218c03b9f7a5
Connor Kam-Cheong <connorkc42@gmail.com>
2019-02-09 23:24:33 +0000

    Add run_fizzbuzz function to test in console

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,5 +3,10 @@ def fizzbuzz(x):
 
     if x % 3 == 0: result += 'fizz'
     if x % 5 == 0: result += 'buzz'
-    if result == '': result = x 
+    if result == '': result = x
     return result
+
+def run_fizzbuzz(i, n):
+    while i <= n:
+        fizzbuzz(i)
+        i += 1
