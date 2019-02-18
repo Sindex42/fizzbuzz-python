49dffebcb8e02b9174c8edc984d9ec72c408ee3e
Connor Kam-Cheong <connorkc42@gmail.com>
2019-02-18 14:35:19 +0000

    Add pylint and autopep8 for linting

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,14 @@
 def fizzbuzz(x):
     result = ''
 
-    if x % 3 == 0: result += 'fizz'
-    if x % 5 == 0: result += 'buzz'
-    if result == '': result = x
+    if x % 3 == 0:
+        result += 'fizz'
+    if x % 5 == 0:
+        result += 'buzz'
+    if result == '':
+        result = x
     return result
+
 
 def run_fizzbuzz(i, n):
     while i <= n:
